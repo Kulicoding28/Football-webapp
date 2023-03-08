38b15e32bad6bc50ebcb7b5668d7155aac188ded
Ahmad Aziz <ahmadabdulaziz380@gmail.com>
2023-03-08 21:09:25 +0700

    page1-done-responsvie

--- src/components/fieldList/FieldList.jsx
+++ src/components/fieldList/FieldList.jsx
@@ -7,27 +7,27 @@ const FieldList = () => {
       <div className={styles.fListItem}>
         <img src="/assets/card1.png" alt="" className={styles.fListImg} />
         <div className={styles.fListTitle}>
-          <h1>Batavia Field</h1>
+          <h1 className={styles.judul}>Batavia Field</h1>
         </div>
       </div>
 
       <div className={styles.fListItem}>
         <img src="/assets/card2.png" alt="" className={styles.fListImg} />
         <div className={styles.fListTitle}>
-          <h1>Ragunan Field</h1>
+          <h1 className={styles.judul}>Ragunan Field</h1>
         </div>
       </div>
 
       <div className={styles.fListItem}>
         <img src="/assets/card3.png" alt="" className={styles.fListImg} />
         <div className={styles.fListTitle}>
-          <h1>Kukusan Field</h1>
+          <h1 className={styles.judul}>Kukusan Field</h1>
         </div>
       </div>
       <div className={styles.fListItem}>
         <img src="/assets/card4.png" alt="" className={styles.fListImg} />
         <div className={styles.fListTitle}>
-          <h1>Kemang Field</h1>
+          <h1 className={styles.judul}>Kemang Field</h1>
         </div>
       </div>
     </div>
